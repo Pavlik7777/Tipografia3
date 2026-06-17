c2f113adef091ba9e978605648fd9d1a6d268982
Pavlik7777 <Pasha20080620@gmail.com>
2026-06-17 12:00:01 +0200

    TipografiaContext.cs

--- Models/TipografiaContext.cs
+++ Models/TipografiaContext.cs
@@ -28,15 +28,6 @@ public partial class TipografiaContext : DbContext
     public virtual DbSet<Product> Products { get; set; }
 
     public virtual DbSet<TipografCeh> TipografCehs { get; set; }
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    //// Путь к БД в папке проекта (рядом с .exe)
-    //    //var dbPath = Path.Combine(
-    //    //    AppDomain.CurrentDomain.BaseDirectory,
-    //    //    "Tipografia.db");
-
-    //    //optionsBuilder.UseSqlite($"Data Source={dbPath}");
-    //}
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlite("Data Source=Tipografia.db");
